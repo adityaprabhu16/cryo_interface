7476c2f9b32e6755727c13894531730c1871e83e
dunca384 <dunca384@umn.edu>
2023-03-15 10:08:34 -0500

    print temperature data from thread

--- app_thread.py
+++ app_thread.py
@@ -1,5 +1,7 @@
 
+import serial
 from threading import Thread
+import time
 
 
 class AppThread(Thread):
@@ -7,11 +9,27 @@ class AppThread(Thread):
     def __init__(self):
         super().__init__()
         self.killed = False
+        self.con = None
 
     def run(self):
+
+        port = 'COM3'
+        baudrate = 9600  # Set this to what it will actually be
+        timeout = 5
+
+        self.con = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
+
+        self.con.write('*IDN?\n'.encode('utf-8'))
+        self.con.flush()
+        print(self.con.readline().decode('utf-8').rstrip())
+
         while not self.killed:
-            # TODO: call time.sleep() instead
-            pass
+            self.con.write('GET TEMP\n'.encode('utf-8'))
+            self.con.flush()
+            print(self.con.readline().decode('utf-8').rstrip())
+            time.sleep(1)
     
     def stop(self):
         self.killed = True
+        if self.con:
+            self.con.close()
