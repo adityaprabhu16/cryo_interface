d0ca1873259022fbbe5771f59886d9eba8bd34e8
dunca384 <dunca384@umn.edu>
2023-04-08 12:41:57 -0500

    AppThread documentation

--- app_thread.py
+++ app_thread.py
@@ -1,10 +1,13 @@
 
 import logging
+from metadata import Metadata
 import os
 import queue
 import serial
+import socket
 from threading import Thread
 import time
+from typing import List, Optional
 
 from config import Config
 from vna import build_cmd
@@ -14,19 +17,36 @@ class AppThread(Thread):
 
     def __init__(self):
         super().__init__()
-        self.running = False
-        self.con = None
-        self.queue_pool = []
-        self.metadata = None
-        self.config = Config(period=30.0)
-        self.dir = None
+
+        # Application metadata.
+        self.metadata: Optional[Metadata] = None
+
+        # Application runtime configuration.
+        self.config: Config = Config(period=30.0)
+
+        # Whether an experiment has been selected.
         self.experiment_selected = False
 
+        # Whether or not the experiment is running.
+        self.running = False
+
+        # Serial connection to microcontroller.
+        self.con: Optional[serial.Serial] = None
+
+        # VNA connections.
+        self.vna_con1: Optional[socket.socket] = None
+        self.vna_con2: Optional[socket.socket] = None
+
+        # Temperature data collected by the experiment.
         self.data = []
 
-        self.vna_con1 = None
-        self.vna_con2 = None
+        # Collection of queues used for data streaming.
+        self.queue_pool: List[queue.Queue] = []
 
+        # Directory to store data in.
+        self.dir: Optional[str] = None
+
+        # Wether or not the application has been killed.
         self.killed = False
 
     def run(self):
@@ -34,21 +54,29 @@ class AppThread(Thread):
         # Run forever as long as the thread has not been killed.
         while not self.killed:
 
+            # If the experiment is running.
             if self.running:
+                # Open the file for saving temperature data.
                 with open(os.path.join('experiments', self.dir, 'data.csv'), 'w+') as wf:
+                    # We loop here so check again if the experiment is running and the application is alive.
                     while self.running and not self.killed:
 
+                        # Whether we need to try again at taking data due to an error.
                         retry = False
 
+                        # Get the current time.
                         t = time.time()
 
+                        # If we have a USB connection.
                         if self.con:
                             try:
                                 # Request temperature from ESP32
                                 self.con.write('GET TEMP\n'.encode('utf-8'))
                                 self.con.flush()
 
+                                # Read until the newline character, decode to utf-8, and remove the ending newline character.
                                 data = self.con.readline().decode('utf-8').rstrip()
+                                # Get temperatures from the string.
                                 temp1, temp2 = [float(x) for x in data.split(',')]
 
                                 data = {
@@ -77,7 +105,7 @@ class AppThread(Thread):
                             except:
                                 logging.exception('Exception encountered in app thread.')
                         
-
+                        # If we are connected to VNA 1.
                         if self.vna_con1:
                             try:
                                 self.vna_con1.send(build_cmd('MMEM:DATA? "FTEST.csv"'))
@@ -102,6 +130,7 @@ class AppThread(Thread):
                                     logging.exception('Error closing connection')
                                 self.vna_con1 = None
                         
+                        # If we are connected to VNA 2.
                         if self.vna_con2:
                             try:
                                 self.vna_con2.send(build_cmd('MMEM:DATA? "FTEST.csv"'))
